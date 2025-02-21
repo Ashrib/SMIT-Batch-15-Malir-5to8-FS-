4de6e33280d00ace1509f6d7c046c4a34d4ed337
Ashrib <asharib920@gmail.com>
2025-02-21 19:52:44 +0500

    JS start, variable into

---
+++ JS/1. JS intro/app.js
@@ -0,0 +1,24 @@
+// alert("thankyou for input");
+// window.alert("2nd alert");
+var text = "this is text";
+// document.write(text);
+
+var a;
+a = "abc";
+document.write(a);
+a = "abcd";
+document.write(a);
+
+var _id = "abc";
+var userNameData = "abc";
+var user_name = "abc";
+var alerts = "100";
+
+document.write(alerts);
+
+
+
+
+
+
+
