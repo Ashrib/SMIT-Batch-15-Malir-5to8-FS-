ca5f393e638ce0c0c0b696059affe6f55e2eb65c
Ashrib <asharib920@gmail.com>
2025-04-20 18:52:12 +0500

    color game logic with dom

---
+++ JS/15. color game with DOM/app.js
@@ -0,0 +1,26 @@
+
+
+var score = document.getElementById('score')
+
+function showConsole(e){
+  console.log(e.target.style.backgroundColor  )
+}
+
+var dataArray = ['ali','hamza','usman','maaz']
+var body = document.getElementsByTagName('body')[0]
+var colors = ['red','yellow','green','blue']
+
+for (let i = 0; i < 30; i++) {
+  var randomNum = Math.floor(Math.random()*4)
+  var divElm = document.createElement('div')
+  divElm.className = 'card';
+  divElm.style.backgroundColor = colors[randomNum];  
+  // divElm.innerText = dataArray[i];
+  // divElm.innerText = "Hello"
+  divElm.addEventListener('click', showConsole)
+  
+  body.appendChild(divElm)
+}
+
+
+
