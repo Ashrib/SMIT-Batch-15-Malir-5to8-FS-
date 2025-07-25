f844364caebb6f11b98164a01202631b88b64e93
Ashrib <asharib920@gmail.com>
2025-07-25 19:30:31 +0500

    GSAP animation

---
+++ GSAP/app.js
@@ -0,0 +1,27 @@
+gsap.from(".box", {
+    rotationX: 360,
+    rotationY: 360,
+    // x: 100, 
+    duration: 1,
+    stagger:1,
+    // scaleX: 1.4,
+    // translateX: 5,
+    
+});
+
+
+gsap.fromTo('.img', {
+    opacity: 0,
+    duration: 1.5,rollTrigge
+    
+}, {
+    opacity: 1,
+    duration: 3,
+    scaleX: 0,
+    scaleY: 0,
+}
+
+
+)
+
+
