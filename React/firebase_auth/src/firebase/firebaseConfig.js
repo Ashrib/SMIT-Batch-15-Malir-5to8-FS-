294a2228999a260df8b7abb640ff96f8764d179b
Saima-Imran90 <smalldeeds1234@gmail.com>
2025-09-13 20:03:53 +0500

    config removed

--- React/firebase_auth/src/firebase/firebaseConfig.js
+++ React/firebase_auth/src/firebase/firebaseConfig.js
@@ -8,13 +8,7 @@ import { getAuth, createUserWithEmailAndPassword} from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBG4WqmzXLAXSEw0GVzMBsAB_ON86Yah_Q",
-  authDomain: "smit-firestore-1e367.firebaseapp.com",
-  projectId: "smit-firestore-1e367",
-  storageBucket: "smit-firestore-1e367.firebasestorage.app",
-  messagingSenderId: "559579412068",
-  appId: "1:559579412068:web:81947ea811748a2aec5171",
-  measurementId: "G-2MGR33YMRV"
+  // ...
 };
 
 // Initialize Firebase
